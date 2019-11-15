4ae02be7b0b39e9bf8f00e2a5c3e9106e0408d09
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-11-16 04:33:31 +0900

    :sparkles: Add schedules api.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   get 'schedule_users/:id/new', to: 'schedule_users#new'
   post 'schedules/schedule_users/:id', to: 'schedule_users#create'
 
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :schedules
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
