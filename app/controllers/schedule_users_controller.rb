7ed876ea647033b9a6ade79aea3073bf5e5e8baa
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-11-16 01:02:04 +0900

    :+1: Add new schedule_users forms.

--- app/controllers/schedule_users_controller.rb
+++ app/controllers/schedule_users_controller.rb
@@ -1,10 +1,12 @@
-class ScheduleUserController < ApplicationController
+class ScheduleUsersController < ApplicationController
   def new
-    @schedule = Schedule.new
+    @schedule_user = ScheduleUser.new
   end
 
   def create
     schedule_user = ScheduleUser.new(schedule_user_params)
+    schedule_user.user_id = current_user.id
+    schedule_user.schedule_id = params[:id]
     schedule_user.save!
     redirect_to schedules_url
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  post 'schedule_users/:id'
   resources :schedules
   devise_for :users
+  get 'schedule_users/:id/new', to: 'schedule_users#new'
+  post 'schedules/schedule_users/:id', to: 'schedule_users#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
