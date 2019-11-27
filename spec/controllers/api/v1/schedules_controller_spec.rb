a0963d6a1f6c3d4145d20db37242001e9ba38712
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-11-28 04:20:22 +0900

    :+1: Add test "api/v1/schedule_controller :index"

--- spec/controllers/api/v1/schedules_controller_spec.rb
+++ spec/controllers/api/v1/schedules_controller_spec.rb
@@ -3,38 +3,45 @@ require 'rails_helper'
 RSpec.describe Api::V1::SchedulesController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "正常系ステータスを返す" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "" do
+      FactoryBot.create_list(:post, 10)
+      get :index
+
+    end
+
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #create" do
+  #   it "returns http success" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #update" do
+  #   it "returns http success" do
+  #     get :update
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #destroy" do
+  #   it "returns http success" do
+  #     get :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
